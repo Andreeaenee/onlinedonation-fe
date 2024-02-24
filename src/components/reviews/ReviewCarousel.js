c83875ec073154e847a75b4e4d8d7051252f67a2
Andreea Ene <andreaaene.12@gmail.com>
2024-02-24 16:08:09 +0200

    Setting Timeout for Review on AboutUs page

--- src/components/reviews/ReviewCarousel.js
+++ src/components/reviews/ReviewCarousel.js
@@ -4,15 +4,28 @@ import 'slick-carousel/slick/slick-theme.css'
 import React from 'react'
 import Review from './Review'
 import '../../styles.css'
+import { useRef, useEffect } from 'react'
 
 const ReviewCarousel = ({ reviews }) => {
-  //fix the width of the carousel
+  const sliderRef = useRef(null)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sliderRef.current) {
+        sliderRef.current.slickNext()
+      }
+    }, 5000) // Change the interval time as needed (5000ms = 5 seconds)
+
+    return () => clearInterval(interval)
+  }, [])
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 1024,
@@ -34,7 +47,7 @@ const ReviewCarousel = ({ reviews }) => {
     ],
   }
   return (
-    <Slider {...settings}>
+    <Slider ref={sliderRef} {...settings}>
       {reviews.map((review, index) => (
         <Review key={index} {...review} />
       ))}
